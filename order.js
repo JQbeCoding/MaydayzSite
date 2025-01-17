13feefb333f80d57927198fa9e6a28122090d9ba
Ja'Quis <jaquismay2004@gmail.com>
2025-01-16 19:30:35 -0500

    Added js functions to go to payment links.

--- order.js
+++ order.js
@@ -1,3 +1,23 @@
-function openPayment(){
-   onclick(window.open("https://www.google.com/?client=safari"));
+function openSamplerPayment(){
+   onclick(window.open("https://square.link/u/DiLFGufU"));
+}
+
+function openPlatesPayment(){
+   onclick(window.open("https://square.link/u/LQwii9Tz"))
+}
+
+function openNuggetsPayment(){
+   onclick(window.open("https://square.link/u/d071gIWh"))
+}
+
+function openSandwichPayment(){
+   onclick(window.open("https://square.link/u/Y0cOImSn"))
+}
+
+function openCakePayment(){
+   onclick(window.open("https://www.merriam-webster.com/dictionary/unavailable"))
+}
+
+function openCobblerPayment(){
+   onclick(window.open("https://square.link/u/EWZdQQbf"))
 }
