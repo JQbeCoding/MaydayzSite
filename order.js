f021ef032dc78f1d675805eb634b38a980e5eb26
Ja'Quis <jaquismay2004@gmail.com>
2025-01-19 11:28:37 -0500

    Yes

--- order.js
+++ order.js
@@ -1,35 +1,35 @@
 function openSamplerPayment(){
-   onclick(window.open("https://square.link/u/DiLFGufU",  "Sampler","width=700,height=500"));
+   onclick(window.open("https://square.link/u/DiLFGufU",  "Sampler","width=700,height=500,top=150,left=375"));
 }
 
 function openPlatesPayment(){
-   onclick(window.open("https://square.link/u/LQwii9Tz", "Sampler",  "width=500,height=500"))
+   onclick(window.open("https://square.link/u/LQwii9Tz", "Sampler",  "width=500,height=500,top=150,left=375"))
 }
 
 function openNuggetsPayment(){
-   onclick(window.open("https://square.link/u/d071gIWh", "Nuggets", "width=500,height=500"))
+   onclick(window.open("https://square.link/u/d071gIWh", "Nuggets", "width=500,height=500,top=150,left=375"))
 }
 
 function openSandwichPayment(){
-   onclick(window.open("https://square.link/u/Y0cOImSn","Sanwhich" ,"width=500,height=500"))
+   onclick(window.open("https://square.link/u/Y0cOImSn","Sanwhich" ,"width=500,height=500,top=150,left=375"))
 }
 
 function openCakePayment(){
-   onclick(window.open("https://www.merriam-webster.com/dictionary/unavailable","Unavailable" ,"width=500,height=500"))
+   onclick(window.open("https://www.merriam-webster.com/dictionary/unavailable","Unavailable" ,"width=500,height=500,top=150,left=375"))
 }
 
 function openCobblerPayment(){
-   onclick(window.open("https://square.link/u/EWZdQQbf","Cobbler" ,"width=500,height=500"))
+   onclick(window.open("https://square.link/u/EWZdQQbf","Cobbler" ,"width=500,height=500,top=150,left=375"))
 }
 function openRibPlatePayment(){
-   onclick(window.open("https://square.link/u/06cF8aZF","Rib Plate", "width=500,height=500" ))
+   onclick(window.open("https://square.link/u/06cF8aZF","Rib Plate", "width=500,height=500,top=150,left=375" ))
 }
 function openPulledPorkPayment(){
-   onclick(window.open("https://square.link/u/WPbCdn1F", "Pulled Pork", "width=500,height=500"))
+   onclick(window.open("https://square.link/u/WPbCdn1F", "Pulled Pork", "width=500,height=500,top=150,left=375"))
 }
 function openPorkChopPayment(){
-   onclick(window.open("https://square.link/u/5GRCttGm", "Pork Chop Plate", "width=500,height=500"))
+   onclick(window.open("https://square.link/u/5GRCttGm", "Pork Chop Plate", "width=500,height=500,top=150,left=375"))
 }
 function openPulledChickenPayment(){
-   onclick(window.open("https://square.link/u/D2zoTAFF", "Pork Chop Plate", "width=500,height=500"))
+   onclick(window.open("https://square.link/u/D2zoTAFF", "Pork Chop Plate", "width=500,height=500,top=150,left=375"))
 }
