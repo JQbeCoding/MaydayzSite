c41148f300bf094bf8e555ee305ae8f1da2687a9
Ja'Quis <jaquismay2004@gmail.com>
2025-01-18 21:44:42 -0500

    Changing the payment functions

--- order.js
+++ order.js
@@ -1,23 +1,32 @@
 function openSamplerPayment(){
-   onclick(window.open("https://square.link/u/DiLFGufU"));
+   onclick(window.open("https://square.link/u/DiLFGufU",  "Sampler","width=700,height=500"));
 }
 
 function openPlatesPayment(){
-   onclick(window.open("https://square.link/u/LQwii9Tz"))
+   onclick(window.open("https://square.link/u/LQwii9Tz", "Sampler",  "width=500,height=500"))
 }
 
 function openNuggetsPayment(){
-   onclick(window.open("https://square.link/u/d071gIWh"))
+   onclick(window.open("https://square.link/u/d071gIWh", "Nuggets", "width=500,height=500"))
 }
 
 function openSandwichPayment(){
-   onclick(window.open("https://square.link/u/Y0cOImSn"))
+   onclick(window.open("https://square.link/u/Y0cOImSn","Sanwhich" ,"width=500,height=500"))
 }
 
 function openCakePayment(){
-   onclick(window.open("https://www.merriam-webster.com/dictionary/unavailable"))
+   onclick(window.open("https://www.merriam-webster.com/dictionary/unavailable","Unavailable" ,"width=500,height=500"))
 }
 
 function openCobblerPayment(){
-   onclick(window.open("https://square.link/u/EWZdQQbf"))
+   onclick(window.open("https://square.link/u/EWZdQQbf","Cobbler" ,"width=500,height=500"))
+}
+function openRibPlatePayment(){
+   onclick(window.open("https://square.link/u/06cF8aZF","Rib Plate", "width=500,height=500" ))
+}
+function openPulledPorkPayment(){
+   onclick(window.open("https://square.link/u/WPbCdn1F", "Pulled Pork", "width=500,height=500"))
+}
+function openPorkChopPayment(){
+   onclick(window.open("https://square.link/u/5GRCttGm", "Pork Chop Plate", "width=500,height=500"))
 }
