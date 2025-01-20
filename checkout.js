a96c0d551f62c287a209b0aaa795612b304c5891
Ja'Quis <jaquismay2004@gmail.com>
2025-01-20 11:59:22 -0500

    word

--- checkout.js
+++ checkout.js
@@ -1,4 +1,5 @@
 function checkoutPlate() {
+    var price = 22;
     const drink = document.querySelector('input[name="drink"]:checked');
     const sides = [...document.querySelectorAll('input[name="side"]:checked')];
     let orderSummary = "Order Summary:\n";
@@ -7,12 +8,12 @@ function checkoutPlate() {
     alert(orderSummary);
 }
 function checkoutSampler() {
+    var price = 32;
     const drink = document.querySelector('input[name="drink"]:checked');
     const sides = [...document.querySelectorAll('input[name="side"]:checked')];
     let orderSummary = "Order Summary:\n";
     orderSummary += `Drink: ${drink ? drink.value : "None"}\n`;
-
-    orderSummary += `meats: ${sides.map(side => side.value).join(", ") || "None"}\n`;
+    orderSummary += `Meats: ${sides.map(side => side.value).join(", ") || "None"}\n`;
     orderSummary += `Sides: ${sides.map(side => side.value).join(", ") || "None"}\n`;
     alert(orderSummary);
 }
