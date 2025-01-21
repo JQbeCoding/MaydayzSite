b08e93356372d47a3be92d7cd740f86145832821
Ja'Quis <jaquismay2004@gmail.com>
2025-01-20 20:38:58 -0500

    Format

--- checkout.js
+++ checkout.js
@@ -1,28 +1,33 @@
 function checkoutPlate() {
-    var price = 22;
-    const drink = document.querySelector('input[name="drink"]:checked');
-    const sides = [...document.querySelectorAll('input[name="side"]:checked')];
-    let orderSummary = "Order Summary:\n";
-    orderSummary += `Drink: ${drink ? drink.value : "None"}\n`;
-    orderSummary += `Sides: ${sides.map(side => side.value).join(", ") || "None"}\n`;
-    alert(orderSummary);
+  var price = 22;
+  const drink = document.querySelector('input[name="drink"]:checked');
+  const sides = [...document.querySelectorAll('input[name="side"]:checked')];
+  let orderSummary = "Order Summary:\n";
+  orderSummary += `Drink: ${drink ? drink.value : "None"}\n`;
+  orderSummary += `Sides: ${
+    sides.map((side) => side.value).join(", ") || "None"
+  }\n`;
+  alert(orderSummary);
 }
 function checkoutSampler() {
-    const price = 32;
-    const drink = document.querySelector('input[name="drink"]:checked');
-    const meats = [...document.querySelectorAll('input[name="meat"]:checked')];
-    const sides = [...document.querySelectorAll('input[name="side"]:checked')];
+  const price = 32;
+  const drink = document.querySelector('input[name="drink"]:checked');
+  const meats = [...document.querySelectorAll('input[name="meat"]:checked')];
+  const sides = [...document.querySelectorAll('input[name="side"]:checked')];
 
-    let orderSummary = "<h2>Order Summary:</h2>";
-    orderSummary += `<p id = "option"><strong>Drink:</strong> ${drink ? drink.value : "None"}</p>`;
-    orderSummary += `<p id = "option"><strong>Meats:</strong> ${meats.length > 0 ? meats.map(meat => meat.value).join(", ") : "None"}</p>`;
-    orderSummary += `<p id = "option"><strong>Sides:</strong> ${sides.length > 0 ? sides.map(side => side.value).join(", ") : "None"}</p>`;
-    orderSummary += `<p id = "option"><strong>Total Price:</strong> $${price}</p>`;
+  let orderSummary = "<h2>Order Summary:</h2>";
+  orderSummary += `<p id = "option"><strong>Drink:</strong> ${
+    drink ? drink.value : "None"
+  }</p>`;
+  orderSummary += `<p id = "option"><strong>Meats:</strong> ${
+    meats.length > 0 ? meats.map((meat) => meat.value).join(", ") : "None"
+  }</p>`;
+  orderSummary += `<p id = "option"><strong>Sides:</strong> ${
+    sides.length > 0 ? sides.map((side) => side.value).join(", ") : "None"
+  }</p>`;
+  orderSummary += `<p id = "option"><strong>Total Price:</strong> $${price}</p>`;
 
-    localStorage.setItem("orderSummary", orderSummary);
+  localStorage.setItem("orderSummary", orderSummary);
 
-    window.location.replace("payment.html");
+  window.location.replace("payment.html");
 }
-
-
-
