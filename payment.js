01de81cc0b0413c849e2a458b5c6e2800bb6f92b
Ja'Quis <jaquismay2004@gmail.com>
2025-02-28 10:56:48 -0500

    Deleting unused Files

--- payment.js
+++
@@ -1,9 +0,0 @@
-try {
-    const response = await client.applePayApi.registerDomain({
-      domainName: 'www.maydayz.com'
-    });
-  
-    console.log(response.result);
-  } catch(error) {
-    console.log(error);
-  }
