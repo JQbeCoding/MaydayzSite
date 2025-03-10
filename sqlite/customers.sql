2f429be737f19603acb778da8b4e50773d05adb0
Ja'Quis <jaquismay2004@gmail.com>
2025-03-10 11:24:36 -0400

    Fixing sql

--- sqlite/customers.sql
+++ sqlite/customers.sql
@@ -18,19 +18,19 @@ SET phoneNumber = '+17048910418'
 WHERE id = 3;
 
 
-INSERT INTO customers (name, phoneNumber) VALUES 
-('Adreanna', '+17047461789'),
-('Aquanda', '+170428121'),
-('Artrice', '+9803789740'),
-('Asonie', '+9808993029'),
-('Patricia', '+17042588307'),
-('Bridgette', '+17046056417'),
-('Schenna', '+19804755909'),
-('Cole', '+17049150310'),
-('Nathdaniel', '+17046172546'),
-('Danny', '+17739324360'),
-('Ernest', '+17042588842'),
-('J Boog', '+18646339230'),
+-- INSERT INTO customers (name, phoneNumber) VALUES 
+--('Adreanna', '+17047461789'),
+--('Aquanda', '+170428121'),
+--('Artrice', '+9803789740'),
+--('Asonie', '+9808993029'),
+--('Patricia', '+17042588307'),
+--('Bridgette', '+17046056417'),
+--('Schenna', '+19804755909'),
+--('Cole', '+17049150310'),
+--('Nathdaniel', '+17046172546'),
+--('Danny', '+17739324360'),
+--('Ernest', '+17042588842'),
+/*('J Boog', '+18646339230'),
 ('Jay', '+17044498350'),
 ('Jevon', '+19802642071'),
 ('Josh', '+17047433479'),
@@ -45,9 +45,27 @@ INSERT INTO customers (name, phoneNumber) VALUES
 ('Wanda', '+17047803377'),
 ('Kesha', '+17046847016'),
 ('Patrick', '+17049016859'),
-('Bryan', '+19804155465');
+('Bryan', '+19804155465');*/
 
 INSERT INTO customers (name, phoneNumber) 
 VALUES ('Taylor', '+17049495173');
+
+/*(UPDATE customers 
+SET phoneNumber ='7042812121'
+WHERE id = 5;*/
+
+UPDATE customers 
+SET phoneNumber ='+17042812121'
+WHERE id = 5;
+
+
+UPDATE customers 
+SET phoneNumber ='+19803789740'
+WHERE id = 6;
+
+
+UPDATE customers 
+SET phoneNumber ='+19808993029'
+WHERE id = 7;
 
 SELECT * FROM customers;
