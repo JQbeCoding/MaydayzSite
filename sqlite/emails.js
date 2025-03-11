4d3429638d1ad3f3df4a3e29babb3f541c51f9f3
Ja'Quis <jaquismay2004@gmail.com>
2025-03-10 20:32:12 -0400

    Email

--- sqlite/emails.js
+++ sqlite/emails.js
@@ -1,4 +1,3 @@
-
 import "dotenv/config";
 import nodemailer from "nodemailer";
 import sqlite3 from "sqlite3";
@@ -38,8 +37,35 @@ async function sendEmail() {
       let mailOptions = {
         from: process.env.EMAIL_USER,
         to: recipientEmail, // Retrieved from database
-        subject: "WE SMOKED OUT",
-        html: "<h1>We’ve got some delicious BBQ ready for you!</h1>", // Add HTML content here
+        subject: "SMOKED MAIL",
+        html: `
+        <!DOCTYPE html>
+        <html>
+        <body>
+          <h1 style="text-align: center">MAYDAYZ FAMILY</h1>
+          <h2 style="text-align: center">Welcome to the Smoked Out mail</h2>
+          <p style="text-align: center">
+            This email service will be the hub for menus,<br />
+            updates, and even <strong>EXCLUSIVE</strong> deals.<br />
+            We are going to have a lot of fun. As always.<br />
+            <strong style="font-size: larger">WE SMOKED OUT!!!</strong>
+          </p>
+          <img
+            src="cid:maydayzlogo"
+            height="100px"
+            width="100px"
+            style="display: block; margin: 0 auto"
+          />
+        </body>
+        </html>
+        `,
+        attachments: [
+          {
+            filename: "Maydayz (64 x 64 px) (Logo).png",
+            path: "/Users/jaquismay/Documents/GitHub/MaydayzSite/src/css/assets/Images/Maydayz (64 x 64 px) (Logo).png",
+            cid: "maydayzlogo",
+          },
+        ],
       };
 
       // Send the email
