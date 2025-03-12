34be1ab9892abc1c3ad1c765e46f96a13fc8536a
Ja'Quis <jaquismay2004@gmail.com>
2025-03-12 16:01:30 -0400

    Loading Screen Requirements

---
+++ src/js/loading.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    const loadingScreen = document.getElementById('loading-screen');
+    const content = document.getElementById('content');
+
+    loadingScreen.style.display = 'none';
+    content.style.display = 'block'; 
+});
